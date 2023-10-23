1c283694e225b5a59ab0f5fb81b45de7e7f9b3a5
Gatsbeau <jabriel0ne@gmail.com>
2023-10-23 13:10:26 -0400

    Added prompt question array

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markGen = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name:'description',
-        message: 'Write a description of your project.',
-    }
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your application?",
+  },
+  { type: "input", name: "usage", message: "How do you use your project?" },
+  {
+    type: "list",
+    name: "license",
+    message: "What license are you using for your project?",
+    choices: ["Apache", "Boost", "MIT", "none"],
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "List some contribution guidelines.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Include tests for you application",
+  },
 ];
 
 // TODO: Create a function to write README file
